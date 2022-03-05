68f766439691d2daea9b2ccd6962df1a168adf17
Kia <kiapopat@gmail.com>
2022-03-05 22:54:42 +0000

    scanner working

--- hackherthon/screens/shopping.js
+++ hackherthon/screens/shopping.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const speak = () => {
+    const thingToSay = 'hello';
+    Speech.speak(thingToSay);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
 });
