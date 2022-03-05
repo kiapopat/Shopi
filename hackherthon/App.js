7617a9dc47cd60b5772940760aa278a49221661e
Kia <kiapopat@gmail.com>
2022-03-05 19:16:34 +0000

    bottom navigation almost done

--- hackherthon/App.js
+++ hackherthon/App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Expiry from "./screens/expiry.js";
+import Foodfind from "./screens/foodfind.js";
+import Fridge from "./screens/fridge.js";
+import Recipes from "./screens/recipes.js";
+import Donate from "./screens/donate.js";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions ={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+             if (route.name === 'Shopping') {
+               return (
+                 <Ionicons
+                   name={'book-outline'}
+                   size={size}
+                   color={color}
+                 />
+               );
+             } else if (route.name === 'Expiry') {
+              return (
+                <Ionicons
+                  name= {"cog-outline"}
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Fridge') {
+              return (
+                <Ionicons
+                  name={'bookmark-outline'}
+                  size={size}
+                  color={color}
+                />
+              );
+            } 
+
+            else if (route.name === 'Recipes') {
+              return (
+                <Ionicons
+                  name={'person-outline'}
+                  size={size}
+                  color={color}
+                />
+              );
+            } 
+            else if (route.name === 'Donate') {
+              return (
+                <Ionicons
+                  name={'chatbox-outline'}
+                  size={size}
+                  color={color}
+               />
+              );
+            } 
+           },
+          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: 'pink', 
+        })}
+        >
+        { 
+          <Tab.Screen
+          name="Shopping"
+          component={Foodfind}
+        /> 
+        }
+        
+        {
+        <Tab.Screen 
+          name="Scanner" 
+          component={Expiry}
+        />
+        }
+         
+        {  <Tab.Screen 
+          name="Pantry" 
+          component={Fridge} 
+          options={{ tabBarBadge: 4 }}
+          />
+        }
+        
+        {  <Tab.Screen 
+          name="Recipes" 
+          component={Recipes} /> 
+        }
+
+        {
+          <Tab.Screen 
+          name="Donate" 
+          component={Donate} 
+        />
+        }
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
